144f673cf288a89132f45d9d1f4558928e5fecc4
Juan Camilo Bages <jcbages@outlook.com>
2017-09-18 10:15:50 +0000

    added skeleton for login/logout/signup functions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// POST signup a new user
+router.post('/signup', (req, res, next) => {
+  res.json({});
+});
+
+// POST login an existing user
+router.post('/login', (req, res, next) => {
+    res.json({});
+});
+
+// POST logout a user
+router.post('/logout', (req, res, next) => {
+    res.json({});
 });
 
 module.exports = router;
