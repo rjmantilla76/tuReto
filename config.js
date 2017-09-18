93b3b0bf78bb59a514cced460f91378ea82f185b
Juan Camilo Bages <jcbages@outlook.com>
2017-09-18 20:44:28 +0000

    moved creds to vars

---
+++ config.js
@@ -0,0 +1,8 @@
+// define some oauth keys
+module.exports = {
+  github: {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+  }
+};
